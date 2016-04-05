070c72d7e54941c1583898fbe4aab565d06544a0
Farid <farid@Farids-MacBook-Pro.local>
2016-04-05 09:09:20 -0700

    show intercom for soothing dental only. also use cross domain stuff for tracking soothing.doxi.io on www.soothing.dental

--- wp-content/plugins/add_ga/add_analytics.php
+++ wp-content/plugins/add_ga/add_analytics.php
@@ -31,7 +31,8 @@ if(function_exists('override_add_doxi_analytics')) {
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 
-      ga("create", "UA-66473329-1", "auto");
+      ga("create", "UA-66473329-1", "auto", {'allowLinker' : true});
+      if (document.location.host=="www.soothing.dental"){ga('require', 'linker');ga('linker:autoLink', ['soothing.doxi.io'], true, true);};
       ga("send", {
         "hitType": "pageview",
         "page" : window.location.pathname + window.location.search,
@@ -43,7 +44,7 @@ if(function_exists('override_add_doxi_analytics')) {
       app_id: "e6dx8ayb"
       };
         </script>
-        <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic("reattach_activator");ic("update",intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement("script");s.type="text/javascript";s.async=true;s.src="https://widget.intercom.io/widget/e6dx8ayb";var x=d.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent("onload",l);}else{w.addEventListener("load",l,false);}}})()</script>
+        <script>(function(){if (document.location.host!="www.soothing.dental"){return};var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic("reattach_activator");ic("update",intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement("script");s.type="text/javascript";s.async=true;s.src="https://widget.intercom.io/widget/e6dx8ayb";var x=d.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent("onload",l);}else{w.addEventListener("load",l,false);}}})()</script>
         ';
     }
 
